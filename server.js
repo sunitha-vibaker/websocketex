ee9c9e61a1337aadf382f108d3f98ea6376aba83
sunitha <sunitha.vibaker@gmail.com>
2018-12-10 15:52:49 +0530

    web

--- server.js
+++ server.js
@@ -13,13 +13,37 @@ const server = express()
 
 const wss = new SocketServer({ server });
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
-});
 
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 1000);
+
+
+
+
+wss.on('connection', function(ws) {
+
+    ws.on('message', function(message) {
+        message = JSON.parse(message);
+        console.log(message);
+        if(message.type == 'name'){
+            ws.personName = message.data;
+            return;
+        }
+        console.log('Recieved:' +message); 
+        
+        wss.clients.forEach(function e(client){
+            if(client != ws)
+            client.send(JSON.stringify({
+                name:ws.personName,
+                data:message.data
+            }));
+        })
+        
+        //ws.send('From Server: '+message);touc
+        
+
+    });
+    ws.on('close', function() {
+        console.log("I lost a Client");
+
+    });
+    console.log("One more client connected");
+});
